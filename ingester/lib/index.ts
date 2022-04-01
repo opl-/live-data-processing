d40d221c88af340683e13e2f4c536d54c17a7259
opl- <opl-@users.noreply.github.com>
2022-04-01 14:23:35 +0200

    WIP: amqp

--- ingester/lib/index.ts
+++ ingester/lib/index.ts
@@ -3,7 +3,9 @@ export {Pipe} from './Pipe';
 export {Sink} from './Sink';
 export {Tap} from './Tap';
 
+export {AmqpSink, AmqpSinkOpts} from './sink/AmqpSink';
 export {FileSink, FileSinkOpts} from './sink/FileSink';
 export {PostgresSink, PostgresSinkOpts} from './sink/PostgresSink';
 
+export {AmqpTap, AmqpTapOpts, QueueBind} from './tap/AmqpTap';
 export {WebSocketTap, WebSocketTapOpts} from './tap/WebSocketTap';
